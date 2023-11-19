4ddca9b836c16bee1b8305740f3d546d0fb794c0
esinyavin <esinyavin01@gmail.com>
2023-11-19 12:57:34 -0800

    scaled parsing_git_commits to all vulns -- not tested yet

---
+++ rustsec-analysis/parsing_git_commits.py
@@ -0,0 +1,165 @@
+import requests
+import datetime
+
+access_token = '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'
+
+#Set up headers with the access token
+headers = {
+    'Authorization': f'token {access_token}',
+    'Accept': 'application/vnd.github.v3+json'
+}
+
+def compare_cargo_versions(A, B):
+    """
+        A (str): The first version number as a string.
+        B (str): The second version number as a string.
+    Returns:
+        int: -1 if version A comes before version B, 0 if they're equal, and 1 if version A comes after version B.
+    """
+    version_A = list(map(int, A.split('.')))
+    version_B = list(map(int, B.split('.')))
+
+    # Compare each component of the version
+    for a, b in zip(version_A, version_B):
+        if a < b:
+            return -1
+        elif a > b:
+            return 1
+    return 0
+
+#returns -1 if date1 came before date2, 0 if theyre the same, 1 if date1 came after date2
+def compare_date_strings(date1, date2):
+    format_str = '%Y-%m-%dT%H:%M:%SZ'
+    
+    # Convert date strings to datetime objects
+    date1 = datetime.strptime(date_str1, format_str)
+    date2 = datetime.strptime(date_str2, format_str)
+
+    # Perform the comparison
+    if date1 < date2:
+        return -1
+    elif date1 == date2:
+        return 0
+    else:
+        return 1
+
+def get_all_commits(owner, repo):
+    # Define the API endpoint
+    url = f'https://api.github.com/repos/{owner}/{repo}/commits'
+
+    # Make the GET request
+    response = requests.get(url, headers=headers)
+
+    # Check the status code
+    if response.status_code == 200:
+        # Successful request
+        commits = response.json()
+        return commits
+    else:
+        # Error handling
+        print(f"Error: {response.status_code}")
+        print(response.text)
+        return None
+    
+def get_commit_details(commit_url):
+    # Set up headers with the access token
+
+    # Make the GET request to the commit URL
+    response = requests.get(commit_url, headers=headers)
+
+    # Check the status code
+    if response.status_code == 200:
+        # Successful request
+        commit_details = response.json()
+        return commit_details
+    else:
+        # Error handling
+        print(f"Error: {response.status_code}")
+        print(response.text)
+        return None
+
+# Replace these with your own values
+owner = 'jaredforth' #'libwebp-sys'
+repo = 'webp' #NoXF
+vulnerable_dependency = 'libwebp-sys'
+patched_version = '0.9.3'
+
+#return object {"found_patch": T/F, "dependency_version_old": , "dependency_version_new": , "new_version": , "time_upgrade": , "commit_msg" }
+def get_info_about_dependency(owner, repo, vulnerable_dependency, patched_version):
+    # Get all commits from the specified repository
+    all_commits = get_all_commits(owner, repo)
+
+    # Print commit information
+    res = {"found_patch":False}
+    
+    if all_commits:
+        for commit in all_commits:
+            # print(f"Commit SHA: {commit['sha']}")
+            # print(f"Author: {commit['commit']['author']['name']}")
+            # print('-' * 50)
+
+            commit_details = get_commit_details(commit['url'])
+
+            files_affected = commit_details['files']
+            if files_affected:
+                for file_info in files_affected:
+                    if file_info['filename'] == 'Cargo.toml':
+                        if 'patch' in file_info:
+                            s = "+" + vulnerable_dependency
+                            if s in file_info['patch']:
+                                index_dependency = file_info['patch'].find(s)
+                                
+                                substr = file_info['patch'][index_dependency+len(vulnerable_dependency)+4:]
+                                dependency_version = (substr[:substr.find('\n')]).strip('\"')
+
+                                if compare_cargo_versions(dependency_version, patched_version) >= 0:
+                                    
+                                    #print("found upgrade to patched version of dependency")
+                                    res["found_patch"] = True
+
+                                    #if we've captured a commit already, continue if this date came after the one we have. otherwise, override old info.
+                                    if res["time_upgrade"] != None and compare_date_strings(commit['commit']['author']['date'], res["time_upgrade"]) > -1:
+                                        break
+
+                                    res["time_upgrade"] = commit['commit']['author']['date']
+                                    res["commit_msg"] = commit['commit']['message']
+                                    res["dependency_version_new"] = dependency_version
+
+                                    # print(f"Date: {commit['commit']['author']['date']}")
+                                    # print(f"Message: {commit['commit']['message']}")
+                                    k = "-" + vulnerable_dependency
+                                    index_old_version_dependency = file_info['patch'].find(k) + len(k) + 4
+                                    #print('old version of dependency: ')
+                                    res["dependency_version_old"] = file_info['patch'][index_old_version_dependency:index_old_version_dependency+5]
+                                    #print(file_info['patch'][index_old_version_dependency:index_old_version_dependency+5])
+                                    
+                                    p = repo + '\"\n-version'
+                                    #the same commit diff may include the new version of the dependent
+                                    if p in file_info['patch']:
+                                        #print("dependent version changed to ")
+                                        j = "+version = \""
+                                        index_new_version = file_info['patch'].find(j) + len(j)
+                                        res["new_version"] = file_info['patch'][index_new_version:index_new_version+5]
+                                        #print(file_info['patch'][index_new_version:index_new_version+5])
+        
+    return res
+
+#get_info_about_dependency(owner, repo, vulnerable_dependency, patched_version)
+
+#data to collect:
+#Cargo.lock - see edits to it 
+# https://stackoverflow.com/questions/40393117/getting-file-diff-with-github-api
+
+
+#goal: CVE->dependents->github of dependent packages/projects->all commits->diff for each commit in cargo.lock file->find package number
+
+#example:
+#https://rustsec.org/advisories/RUSTSEC-2023-0061.html 
+#https://crates.io/crates/libwebp-sys - vulnerable library
+#https://crates.io/crates/webp - dependent
+#github of dependent: https://github.com/jaredforth/webp 
+
+
+
+#check for multiple branches -- actually just take first edit
+#if a dependent appears not to be patched, it could be because it was using a version less than the "introduced" version
--- rustsec-analysis/rustsec-processing.py
+++ rustsec-analysis/rustsec-processing.py
@@ -1,12 +1,45 @@
-from typing import Union
+
 import os
 import json
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-from cvss import CVSS2, CVSS3 # converts CVSS strings to score
+import requests
+import base64
+import csv
+import parsing_git_commits
+from urllib.parse import urlparse
 
+access_token = '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'
 
+#Set up headers with the access token
+headers = {
+    'Authorization': f'token {access_token}',
+    'Accept': 'application/vnd.github.v3+json'
+}
+
+def get_cargo_toml_content(owner, repo):
+    # url = f'https://crates.io/api/v1/crates?q={cargo_package_name}'
+    # response = requests.get(url, headers={'User-Agent': 'esinyavin01@gmail.com'})
+    # return response.json()
+    url = f'https://api.github.com/repos/{owner}/{repo}/contents/Cargo.toml'
+    response = requests.get(url, headers=headers)
+    content = response.json()['content']
+    return base64.b64decode(content).decode('utf-8')
+
+def get_dependents(cargo_package_name):
+    url = f'https://crates.io/api/v1/crates/{cargo_package_name}/reverse_dependencies'
+    response = requests.get(url, headers={'User-Agent':'esinyavin01@gmail.com'})
+    if response.status_code == 200:
+        result = {}
+        for version in response.json()['versions']:
+            if version['published_by'] == None or version['published_by'] == None:
+                print("github unavailable for cargo_package_name {cargo_package_name} and dependent {version}")
+                result[version['crate']] = {'downloads':version['downloads'], 'github_url': ""}
+                continue
+            #print(version)
+            result[version['crate']] = {'downloads':version['downloads'], 'github_url': version['published_by']['url']}
+        return result
+    else:
+        return "Failed finding dependents"
+    
 def json_to_row(file_path: str):
     """Given a file path to a json file in the OSV format, returns a populated dataframe."""
     with open(file_path, 'r') as file:
@@ -14,61 +47,120 @@ def json_to_row(file_path: str):
     if "withdrawn" in data:
         return None
     
-    #filter out for not crates.io
-    if not data["affected"] or not data["affected"]["package"] or not data["affected"]["package"]["ecosystem"] or data["affected"]["package"]["ecosystem"] != "crates.io":
-        return
+    res = []
+    if not data["affected"]:
+        return res
     
-    row = []
-    row.append(data["id"])
-    row.append(data["affected"]["package"]["name"])
+    for affected in data["affected"]:
+        #filter out for not crates.io
+        
+        if not affected["package"] or not affected["package"]["ecosystem"] or affected["package"]["ecosystem"] != "crates.io":
+            return
+        
+        row = []
+        row.append(data["id"])
+        row.append(data["published"])
+        name = affected["package"]["name"]
+        row.append(name)
 
+        owner = ""
+        repo = ""
 
-    found_github = False
-    for reference in data["references"]:
-        prefix = "https://github.com/"
-        if prefix in reference["url"]:
-            new_string = reference["url"][len(prefix):]
-            owner = new_string[:new_string.find('/')]
-            row.append(owner)
-            new_string_2 = new_string[new_string.find('/')+1:]
-            repo = new_string_2[:new_string_2.find('/')]
-            row.append(repo)
-            found_github = True
-    if found_github == False:
-        row.append("")
-        row.append("")
+        found_github = False
+        for reference in data["references"]:
+            prefix = "https://github.com/"
+            if prefix in reference["url"]:
+                new_string = reference["url"][len(prefix):]
+                owner = new_string[:new_string.find('/')]
+                row.append(owner)
+                new_string_2 = new_string[new_string.find('/')+1:]
+                repo = new_string_2[:new_string_2.find('/')]
+                row.append(repo)
+                found_github = True
+        if found_github == False:
+            row.append("")
+            row.append("")
+        
+        row.append(affected["package"]["purl"])
+
+        if len(data["severity"]) > 0:
+            row.append(data["severity"])
+        else:
+            row.append([])
+
+        if affected["database_specific"] and affected["database_specific"]["categories"]:
+            row.append(affected["database_specific"]["categories"])
+        else:
+            row.append([])
+
+        if len(affected["ranges"]) > 1:
+            print("len(affected[\"ranges\"]) > 1")
+
+        introduced = ""
+        fixed = ""
+
+        if affected["ranges"] and affected["ranges"][0]["events"] and affected["ranges"][0]["events"]:
+            for event in affected["ranges"][0]["events"]:
+                if "introduced" in event:
+                    introduced = event["introduced"]
+                if "fixed" in event:
+                    fixed = event["fixed"]
+            
+        row.append(introduced)
+        row.append(fixed)
+
+        #get dependents using crates.io API
+        #limitations: only current ones, not historical, must have cargo.toml
+        dependents = get_dependents(name)
+        row.append(dependents)
+        res.append(row)
     
-    row.append(data["affected"]["package"]["purl"])
+    return res
 
-    if data["affected"]["ranges"] and data["affected"]["ranges"]["events"] and data["affected"]["ranges"]["events"]["introduced"]:
-        row.append(data["affected"]["ranges"]["events"]["introduced"])
-    else:
-        row.append("")
+def process_rustsec_jsons():
+    # Folder containing RustSec advisories in the OSV format
+    folder_path = './advisory-db-osv/crates/'
+    # Read in all vulnerabilities
+    first_row = ["id", "published", "name", "gh_owner", "gh_repo", "purl", "severity", "categories", "introduced_version", "patched_version", "dependents"]
 
-    if data["affected"]["ranges"] and data["affected"]["ranges"]["events"] and data["affected"]["ranges"]["events"]["fixed"]:
-        row.append(data["affected"]["ranges"]["events"]["fixed"])
-    else:
-        row.append("")
+    with open('all_vulns_info.cvs', 'w', newline='') as csv_file:
+        csv_writer = csv.writer(csv_file)
+        csv_writer.writerow(first_row)
 
-    #get dependents' via github. all dependents, including historical ones
-
-
+        for filename in os.listdir(folder_path):
+            if filename.endswith('.json'):
+                # Construct the full path to the JSON file
+                file_path = os.path.join(folder_path, filename)
+                # Read the JSON file into a DataFrame
+                osv = json_to_row(file_path)
+                if osv is not None:
+                    for o in osv:
+                        print(o)
+                    csv_writer.writerows(osv)
     
-    
+def get_dependent_patch_info():
+    with open('dependent_patch_info.cvs', 'w', newline='') as csv_file:
+        csv_writer = csv.writer(csv_file)
+        first_row = ["vuln_id", "vuln_package_name", "dependent_name", "dependent_info"]
+        csv_writer.writerow(first_row)
+        with open('all_vulns_info.cvs', 'r') as csv_file:
+            csv_reader = csv.reader(csv_file)
+            for row in csv_reader:
+                id = row[0]
+                package_name = row[2]
+                patched_version = row[9]
+                dependents = json.loads(row[10])
+                for dependent in dependents:
+                    parsed_url = urlparse(url)
+                    # Split the path and get the second component (the username)
+                    path_components = parsed_url.path.split('/')
+                    username = path_components[1] if len(path_components) > 1 else None
+                    dependent_info = parsing_git_commits.get_info_about_dependency(username, dependent, package_name, patched_version)
+                    csv_writer.writerow(id, package_name, dependent, dependent_info)
 
-# Folder containing RustSec advisories in the OSV format
-folder_path = './advisory-db-osv/crates/'
+process_rustsec_jsons() 
 
 
 
-# Read in all vulnerabilities
-first_row = ["id", "name", "owner", "repo", "purl", "introduced_version", "patched_version", "dependents"]
-all_rows = []
-for filename in os.listdir(folder_path):
-    if filename.endswith('.json'):
-        # Construct the full path to the JSON file
-        file_path = os.path.join(folder_path, filename)
-        # Read the JSON file into a DataFrame
-        osv = json_to_row(file_path)
-        if osv is not None:
-            all_rows.append(osv)
+        
+        

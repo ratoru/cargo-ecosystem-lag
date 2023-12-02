fc859e5ccbc22d3110d2ec49fc24d2ecdac8f60d
esinyavin <esinyavin01@gmail.com>
2023-12-02 09:40:51 -0800

    fixed some coed

--- rustsec-analysis/parsing_git_commits.py
+++ rustsec-analysis/parsing_git_commits.py
@@ -1,7 +1,7 @@
 import requests
-import datetime
+from datetime import datetime
 
-access_token = ''
+access_token = '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'
 
 #Set up headers with the access token
 headers = {
@@ -16,8 +16,16 @@ def compare_cargo_versions(A, B):
     Returns:
         int: -1 if version A comes before version B, 0 if they're equal, and 1 if version A comes after version B.
     """
+    print(A)
+    print(B)
     version_A = list(map(int, A.split('.')))
     version_B = list(map(int, B.split('.')))
+
+    # Make sure both version lists have the same number of components
+    while len(version_A) < len(version_B):
+        version_A.append(0)
+    while len(version_B) < len(version_A):
+        version_B.append(0)
 
     # Compare each component of the version
     for a, b in zip(version_A, version_B):
@@ -28,7 +36,7 @@ def compare_cargo_versions(A, B):
     return 0
 
 #returns -1 if date1 came before date2, 0 if theyre the same, 1 if date1 came after date2
-def compare_date_strings(date1, date2):
+def compare_date_strings(date_str1, date_str2):
     format_str = '%Y-%m-%dT%H:%M:%SZ'
     
     # Convert date strings to datetime objects
@@ -84,13 +92,28 @@ repo = 'webp' #NoXF
 vulnerable_dependency = 'libwebp-sys'
 patched_version = '0.9.3'
 
+def get_new_dependency_version(file_info, s):
+    index_dependency = file_info['patch'].find(s)
+    #print(file_info['patch'])
+    substr = file_info['patch'][index_dependency+len(vulnerable_dependency)+4:]
+    #print(substr)
+    dependency_version = (substr[:substr.find('\n')]).strip('\"')
+    #need to account for case where this info is structured as a json
+    if 'version =' in dependency_version:
+        i = dependency_version.find('version =')
+        n = i + len('version =') + 2
+        v = dependency_version[n:]
+        i_ = v.find('\"')
+        dependency_version = v[:i_]
+
+
 #return object {"found_patch": T/F, "dependency_version_old": , "dependency_version_new": , "new_version": , "time_upgrade": , "commit_msg" }
 def get_info_about_dependency(owner, repo, vulnerable_dependency, patched_version):
     # Get all commits from the specified repository
     all_commits = get_all_commits(owner, repo)
 
     # Print commit information
-    res = {"found_patch":False}
+    res = {"dependency_patched":True, "dependent_on_vuln_version": False, "found_patch":False}
     
     if all_commits:
         for commit in all_commits:
@@ -105,20 +128,19 @@ def get_info_about_dependency(owner, repo, vulnerable_dependency, patched_versio
                 for file_info in files_affected:
                     if file_info['filename'] == 'Cargo.toml':
                         if 'patch' in file_info:
-                            s = "+" + vulnerable_dependency
+                            s = "+" + vulnerable_dependency + " "
                             if s in file_info['patch']:
-                                index_dependency = file_info['patch'].find(s)
-                                
-                                substr = file_info['patch'][index_dependency+len(vulnerable_dependency)+4:]
-                                dependency_version = (substr[:substr.find('\n')]).strip('\"')
+                                dependency_version = get_new_dependency_version(file_info, s)
 
+                                #need to make sure that prior version wasn't already a patched version
                                 if compare_cargo_versions(dependency_version, patched_version) >= 0:
                                     
                                     #print("found upgrade to patched version of dependency")
                                     res["found_patch"] = True
+                                    print("found patch!")
 
                                     #if we've captured a commit already, continue if this date came after the one we have. otherwise, override old info.
-                                    if res["time_upgrade"] != None and compare_date_strings(commit['commit']['author']['date'], res["time_upgrade"]) > -1:
+                                    if "time_upgrade" in res and compare_date_strings(commit['commit']['author']['date'], res["time_upgrade"]) > -1:
                                         break
 
                                     res["time_upgrade"] = commit['commit']['author']['date']
--- rustsec-analysis/rustsec-processing.py
+++ rustsec-analysis/rustsec-processing.py
@@ -7,7 +7,7 @@ import csv
 import parsing_git_commits
 from urllib.parse import urlparse
 
-access_token = ''
+access_token = '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'
 
 #Set up headers with the access token
 headers = {
@@ -34,11 +34,35 @@ def get_dependents(cargo_package_name):
                 print("github unavailable for cargo_package_name {cargo_package_name} and dependent {version}")
                 result[version['crate']] = {'downloads':version['downloads'], 'github_url': ""}
                 continue
-            #print(version)
-            result[version['crate']] = {'downloads':version['downloads'], 'github_url': version['published_by']['url']}
+            print(version)
+            result[version['crate']] = {'downloads':version['downloads'], 'github_url': version['published_by']['url'], 'version':version['num']}
         return result
     else:
         return "Failed finding dependents"
+    
+#get minimum version of dependent that can be used (basically ignore ~ and ^). 
+def get_version_of_dependency(dependent, version, dependency):
+    url = f'https://crates.io/api/v1/crates/{dependent}/{version}/dependencies'
+    response = requests.get(url, headers={'User-Agent':'esinyavin01@gmail.com'})
+    if response.status_code == 200:
+        for dep in response.json()["dependencies"]:
+            if dep['crate_id'] == dependency:
+                # if '~' in dep['req']:
+                #     print('~ in req for dependent ' + dependent + ' and dependency ' + dependency)
+                # return dep['req'].replace('^', '')
+                return dep['req'].replace('^', '').replace('~', '')
+    else:
+        return "Failed finding dependency version"
+    
+# def get_latest_version(dependent):
+#     url = f'https://crates.io/api/v1/crates/{dependent}'
+#     response = requests.get(url, headers={'User-Agent':'esinyavin01@gmail.com'})
+#     if response.status_code == 200:
+#         for dep in response.json()["dependencies"]:
+#             if dep['crate_id'] == dependency:
+#                 return dep['req'].replace('^', '').replace('~', '')
+#     else:
+#         return "Failed finding dependency version"
     
 def json_to_row(file_path: str):
     """Given a file path to a json file in the OSV format, returns a populated dataframe."""
@@ -123,7 +147,7 @@ def process_rustsec_jsons():
     # Read in all vulnerabilities
     first_row = ["id", "published", "name", "gh_owner", "gh_repo", "purl", "severity", "categories", "introduced_version", "patched_version", "dependents"]
 
-    with open('all_vulns_info.cvs', 'w', newline='') as csv_file:
+    with open('all_vulns_info2.cvs', 'w', newline='') as csv_file:
         csv_writer = csv.writer(csv_file)
         csv_writer.writerow(first_row)
 
@@ -145,21 +169,48 @@ def get_dependent_patch_info():
         csv_writer.writerow(first_row)
         with open('all_vulns_info.cvs', 'r') as csv_file:
             csv_reader = csv.reader(csv_file)
+            first = True
             for row in csv_reader:
+                if first:
+                    first = False
+                    continue
                 id = row[0]
                 package_name = row[2]
                 patched_version = row[9]
-                dependents = json.loads(row[10])
+                introduced_version = row[8]
+                if introduced_version[-2:] == '-0':
+                    introduced_version = introduced_version[:-2]
+                
+                dependents = json.loads(row[10].replace("\'", "\""))
                 for dependent in dependents:
-                    parsed_url = urlparse(url)
+
+                    parsed_url = urlparse(dependents[dependent]['github_url'])
                     # Split the path and get the second component (the username)
                     path_components = parsed_url.path.split('/')
                     username = path_components[1] if len(path_components) > 1 else None
-                    dependent_info = parsing_git_commits.get_info_about_dependency(username, dependent, package_name, patched_version)
-                    csv_writer.writerow(id, package_name, dependent, dependent_info)
+                    dependent_on_version = get_version_of_dependency(dependent, dependents[dependent]['version'], package_name)
+                    if patched_version == "":
+                        #if less than introduced -- this is where tilda and caret matter -- want to get range -- but let's actually just ignore them altogether 
+                        if parsing_git_commits.compare_cargo_versions(dependent_on_version, introduced_version) == -1:
+                            #dependent on a version that came before the bug was introduced
+                            csv_writer.writerow([id, package_name, dependent, {"dependency_patched":False, "dependent_on_vuln_version":False}])
+                        else:
+                            csv_writer.writerow([id, package_name, dependent, {"dependency_patched":False,  "dependent_on_vuln_version":True}])
+                        continue
 
-process_rustsec_jsons() 
+                    if parsing_git_commits.compare_cargo_versions(dependent_on_version, introduced_version) == -1:
+                        #dependent on a version that came before the bug was introduced
+                        csv_writer.writerow([id, package_name, dependent, {"dependency_patched":True, "dependent_on_vuln_version":False}])
+                    elif parsing_git_commits.compare_cargo_versions(dependent_on_version, patched_version) == -1:
+                        #dependent on a version between introduced and fixed
+                        csv_writer.writerow([id, package_name, dependent, {"dependency_patched":True, "dependent_on_vuln_version":True}])
+                    else:
+                        dependent_info = parsing_git_commits.get_info_about_dependency(username, dependent, package_name, patched_version)
+                        print([id, package_name, dependent, dependent_info])
+                        csv_writer.writerow([id, package_name, dependent, dependent_info])
 
+#process_rustsec_jsons() 
+#get_dependent_patch_info()
 
 
         
